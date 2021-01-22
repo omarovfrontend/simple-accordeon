e7c43c022daaf7c84466b0f2e5758365c5fdf1b0
Shamil Omarov <s_omarov05@mail.ru>
2021-01-22 20:59:29 +0300

    Сделал аккордеон через Js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+document.body.addEventListener('click', function(event) {
+    if (event.target.classList.contains('accordeon__btn')) {
+        event.target.classList.toggle('active');
+    }
+});
